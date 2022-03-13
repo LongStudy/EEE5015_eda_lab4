8c9bdc65aeace2b388aefd62a0a974c318dfbd64
xzy <xzyxzy0224@outlook.com>
2022-03-13 20:03:03 +0800

    3

---
+++ led_lights/led_lights_tb.v
@@ -0,0 +1,36 @@
+
+module led_lights_tb;
+    reg clk;
+    reg rst_n;		 
+    wire [3:0] led;
+
+
+  led_lights u_led_lights(clk,rst_n,led);
+
+  parameter CLK_PERIOD = 20;
+
+
+     initial begin
+          clk = 0;
+          forever begin
+               #(CLK_PERIOD/2) clk = ~clk;
+          end
+     end
+
+     initial begin
+          rst_n = 0;
+          #10 
+          rst_n = 1;
+     end
+
+
+  // watch dog
+  initial begin
+    #400 $finish;
+  end
+
+
+  initial begin
+    $vcdpluson; 
+  end
+endmodule
